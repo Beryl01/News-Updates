23fad539a4a32b6810980902ba00eda9ae65955f
Beryl01 <berylnegesa@gmail.com>
2020-04-28 14:44:34 +0300

    CSS Stying for UI

--- manage.py
+++ manage.py
@@ -9,5 +9,15 @@ manager = Manager(app)
 FLASK_RUN_PORT=8000
 manager.add_command("server", Server)
 
+#unittest in manager instance
+@manager.command
+def test():
+    '''
+    Runs the unittest
+    '''
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
 if __name__ == "__main__":
     manager.run()
